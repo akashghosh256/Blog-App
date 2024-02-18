96a5adb1f7e96269b9f10121f7961e937fe4349f
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2024-02-18 11:47:39 +0530

    added user pages

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+
+
+
+
+//env config
+dotenv.config();
+
+
+//routes import
+const userRoutes = require('./routes/userRoutes.js');
+const blogRoutes = require('./routes/blogRoutes.js');
+
+// MongoDB connection
+connectDB();
+
+
+
+// rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json()); //allows to recieve json data from frontend/client
+app.use(morgan("dev"));
+
+//routes
+// app.get("/", (req, res) => {
+//   res.status(200).send({
+//     message: "Node Server",
+//   });
+// });
+
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/blog', blogRoutes);
+
+
+
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+
+// listen
+app.listen(PORT, () => {
+  console.log(`server running at ${PORT} MODE:${process.env.DEV_MODE}`.bgCyan.white);
+});
